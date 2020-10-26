9173fcb86b6df222bfb80732e7ac311b41e10e79
Eliza Kennelly <elizakennelly@gmail.com>
2020-10-26 18:17:50 -0500

    update employee

--- server.js
+++ server.js
@@ -164,3 +164,49 @@ function addEmployee() {
 
     })
 }
+// Update Employee
+function updateEmployee() {
+    connection.query("SELECT employee.last_name, role.title FROM employee JOIN role ON employee.role_id = role.id;", function (err, res) {
+        // console.log(res)
+        if (err) throw err
+        console.log(res)
+        inquirer.prompt([
+            {
+                name: "lastName",
+                type: "rawlist",
+                choices: function () {
+                    var lastName = [];
+                    for (var i = 0; i < res.length; i++) {
+                        lastName.push(res[i].last_name);
+                    }
+                    return lastName;
+                },
+                message: "What is the Employee's last name? ",
+            },
+            {
+                name: "role",
+                type: "rawlist",
+                message: "What is the Employees new title? ",
+                choices: selectRole()
+            },
+        ]).then(function (val) {
+            var roleId = selectRole().indexOf(val.role) + 1
+            connection.query("UPDATE employee SET WHERE ?",
+                {
+                    last_name: val.lastName
+
+                },
+                {
+                    role_id: roleId
+
+                },
+                function (err) {
+                    if (err) throw err
+                    console.table(val)
+                    start()
+                })
+
+        });
+    });
+
+}
